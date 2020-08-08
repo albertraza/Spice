15729fd183b9b616c06eebff387fe0669dab1c08
Albertraza <arbert.jr@hotmail.com>
2020-08-07 22:59:18 -0400

    add index and create actions and ui

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Extentions;
+using Spice.Models;
 using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class SubCategoryController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -19,7 +22,7 @@ namespace Spice.Areas.Admin.Controllers
         }
 
         // GET INDEX
-        public async  Task<IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             return View(await _context.SubCategories.Include(sc => sc.Category).ToListAsync());
         }
@@ -27,13 +30,51 @@ namespace Spice.Areas.Admin.Controllers
         // GET CREATE
         public async Task<IActionResult> Create()
         {
-            var model = new CategoryAndSubCategoryViewModel() {
+            var model = new CategoryAndSubCategoryViewModel()
+            {
                 Categories = await _context.Categories.ToListAsync(),
                 SubCategories = await _context.SubCategories.OrderBy(sc => sc.Name).Select(sc => sc.Name).Distinct().ToListAsync(),
                 SubCategory = new Models.SubCategory()
             };
 
             return View(model);
+        }
+
+        // POST CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryAndSubCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var NewModel = new CategoryAndSubCategoryViewModel()
+                {
+                    Categories = await _context.Categories.ToListAsync(),
+                    SubCategory = model.SubCategory,
+                    SubCategories = await _context.SubCategories.OrderBy(sc => sc.Name).Select(sc => sc.Name).Distinct().ToListAsync()
+                };
+                return View(NewModel);
+            }
+
+            var category = _context.SubCategories.Include(sc => sc.Category).Where(sc => sc.Name == model.SubCategory.Name && sc.Category.Id == model.SubCategory.CategoryId);
+
+            if (category.Count() > 0)
+            {
+                var NewModel = new CategoryAndSubCategoryViewModel()
+                {
+                    Categories = await _context.Categories.ToListAsync(),
+                    SubCategory = model.SubCategory,
+                    SubCategories = await _context.SubCategories.OrderBy(sc => sc.Name).Select(sc => sc.Name).Distinct().ToListAsync(),
+                    StatusMessage = "There's a sub category registered with the same name in the same category, use a different name"
+                };
+                return View(NewModel);
+            }
+            else
+            {
+                _context.SubCategories.Add(model.SubCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 }
---
+++ Spice/Extentions/IEnumarableExtentions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Spice.Extentions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice
+{
+    public static class IEnumarableExtentions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedItem)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedItem.ToString())
+                   };
+        }
+    }
+}
