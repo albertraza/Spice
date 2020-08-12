a81daf3f41836011935af34efca4f7de93f2c482
Albertraza <arbert.jr@hotmail.com>
2020-08-12 12:36:16 -0400

    add create post action

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -9,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models;
 using Spice.Models.ViewModels;
+using Spice.Utils;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -40,6 +42,47 @@ namespace Spice.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(MenuItemViewModel);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost() {
+            MenuItemViewModel.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+                return View(MenuItemViewModel);
+
+            _context.MenuItems.Add(MenuItemViewModel.MenuItem);
+            await _context.SaveChangesAsync();
+
+            //Work on the image saving section
+
+            string webRootPath = _hostingEnviroment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = await _context.MenuItems.FindAsync(MenuItemViewModel.MenuItem.Id);
+
+            if (files.Count > 0)
+            {
+                var uploadPath = Path.Combine(webRootPath, "images");
+                var fileExtention = Path.GetExtension(files[0].FileName);
+
+                using var fileStream = new FileStream(Path.Combine(uploadPath, menuItemFromDb.Id + fileExtention), FileMode.Create);
+                files[0].CopyTo(fileStream);
+
+                menuItemFromDb.Image = @"\images\" + menuItemFromDb.Id + fileExtention;
+
+            }
+            else
+            {
+                var fileExtention = Path.Combine(webRootPath, @"images\" + Util.DefaultImage);
+                System.IO.File.Copy(fileExtention, webRootPath + @"\images\" + menuItemFromDb.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + menuItemFromDb.Id + ".png";
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Spice/Utils/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Utils
+{
+    public static class Util
+    {
+        public const string DefaultImage = "food.png";
+    }
+}
