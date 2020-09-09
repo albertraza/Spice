88eba5b01d282e50208f7e403597785191b00d58
Albertraza <arbert.jr@hotmail.com>
2020-09-09 17:51:17 -0400

    add edit, details and delete

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -60,5 +60,97 @@ namespace Spice.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Coupon == null)
+                return NotFound();
+
+            return View(Coupon);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost, ActionName("Edit")]
+        public async Task<IActionResult> EditPost()
+        {
+            if (!ModelState.IsValid)
+                return View(Coupon);
+
+            var files = HttpContext.Request.Form.Files;
+            var couponFromDb = await _context.Coupons.FirstOrDefaultAsync(c => c.Id == Coupon.Id);
+
+            if (couponFromDb == null)
+                return NotFound();
+
+            if (files.Count > 0)
+            {
+                byte[] pictureFile = null;
+                using var fileStream = files[0].OpenReadStream();
+                using var memoryStream = new MemoryStream();
+                fileStream.CopyTo(memoryStream);
+                pictureFile = memoryStream.ToArray();
+                couponFromDb.Picture = pictureFile;
+            }
+
+            couponFromDb.Name = Coupon.Name;
+            couponFromDb.CouponType = Coupon.CouponType;
+            couponFromDb.Discount = Coupon.Discount;
+            couponFromDb.MinimumAmout = Coupon.MinimumAmout;
+            couponFromDb.IsActive = Coupon.IsActive;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Coupon == null)
+                return NotFound();
+
+            return View(Coupon);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Coupon == null)
+                return NotFound();
+
+            return View(Coupon);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Coupon == null)
+                return NotFound();
+
+            _context.Coupons.Remove(Coupon);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
